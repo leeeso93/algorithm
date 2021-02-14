fb305bcf6e4a8bf4715ae360a9d07c0c60873df8
leeeso93 <leeeso93@gmail.com>
2021-02-14 23:17:12 +0900

    선택 정렬 주석 추가

--- selection_sort.js
+++ selection_sort.js
@@ -1,3 +1,5 @@
+// 선택 정렬 (selection sort)
+// 처리되지 않은 데이터 중에서 가장 작은 데이터를 선택해 맨 앞의 데이터와 바꾸는 것을 반복하는것 
 let arr = [7,5,9,0,3,1,6,2,4,8];
 let n = arr.length;
 
@@ -16,6 +18,7 @@ for(let i = 0; i< n; i++){
     arr[min_index] = temp;
 }
 
+// 출력
 for(let  i =0; i< n; i++){
     console.log(arr[i] + " ");
 }
